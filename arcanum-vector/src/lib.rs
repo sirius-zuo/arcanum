a1e1f16a56890ab181439cdc76390580a8740e72
Jin Zuo <zuojin@gmail.com>
2026-05-29 03:31:21 -0400

    feat(vector): add SQLite MetadataStore and CollectionManager

--- arcanum-vector/src/lib.rs
+++ arcanum-vector/src/lib.rs
@@ -1,4 +1,8 @@
 mod bm25;
+mod collection;
 mod lancedb_store;
+mod metadata;
 pub use bm25::Bm25Index;
+pub use collection::{CollectionManager, CollectionMeta};
 pub use lancedb_store::LanceDbStore;
+pub use metadata::SqliteMetadataStore;
